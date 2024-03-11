9d2a7facc7b67a786fa23023a970c40c47a3b5cd
SitingT <tang.sit@northeastern.edu>
2024-03-10 17:56:48 -0700

    add drop

--- drop.sql
+++ drop.sql
@@ -1,3 +1,19 @@
+DELIMITER $$
+
+-- Drop triggers
+DROP TRIGGER IF EXISTS UpdateAuctionStatusAfterTransaction$$
+DROP TRIGGER IF EXISTS UpdateUserOverallRatingAfterReviewUpdate$$
+
+-- Drop procedures
+DROP PROCEDURE IF EXISTS BlockUser$$
+
+-- Reset the delimiter back to semicolon if you changed it
+DELIMITER ;
+
+-- Drop views
+DROP VIEW IF EXISTS UserOverview;
+
+-- Drop tables. It's crucial to start with tables that have foreign keys pointing to them
 DROP TABLE IF EXISTS OfflinePayments;
 DROP TABLE IF EXISTS Recommendations;
 DROP TABLE IF EXISTS Reports;
@@ -9,3 +25,4 @@ DROP TABLE IF EXISTS Bids;
 DROP TABLE IF EXISTS WhiskyDetails;
 DROP TABLE IF EXISTS PaymentMethods;
 DROP TABLE IF EXISTS Users;
+
