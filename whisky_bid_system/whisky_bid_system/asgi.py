74658ccf0cf26d2a5dc5dd7b073a3806b9fee260
Jessica <s92jca126@gmail.com>
2024-03-30 14:58:01 -0700

    auth login/signup

---
+++ whisky_bid_system/whisky_bid_system/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for whisky_bid_system project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whisky_bid_system.settings')
+
+application = get_asgi_application()
