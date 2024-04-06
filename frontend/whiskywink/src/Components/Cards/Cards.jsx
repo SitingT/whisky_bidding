53c238f267711bb8887e2b9f6153f02cd2dffea2
SitingT <tang.sit@northeastern.edu>
2024-04-05 23:45:10 -0700

    cards

---
+++ frontend/whiskywink/src/Components/Cards/Cards.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  CardMedia,
+  IconButton,
+} from "@mui/material";
+import ImageButtonAsset from "../Assets/cart_icon.png"; // Your cart icon
+import DefaultImage from "../Assets/product_1.png"; // Default image if none is provided
+
+const WhiskyCard = ({ whisky, image }) => (
+  <Card
+    sx={{
+      width: 370,
+      display: "flex",
+      flexDirection: "column",
+    }}
+  >
+    <CardMedia
+      component="img"
+      height="160"
+      sx={{
+        width: "auto",
+        maxWidth: "100%",
+        objectFit: "contain",
+      }}
+      image={image || DefaultImage}
+      alt="Whisky"
+    />
+    <CardContent sx={{ flexGrow: 1 }}>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        Start Price: ${whisky.StartPrice}
+      </Typography>
+      <Typography variant="body2">
+        Category: {whisky.Category}
+        <br />
+        Start Time: {new Date(whisky.StartTime).toLocaleString()}
+        <br />
+        End Time: {new Date(whisky.EndTime).toLocaleString()}
+        <br />
+        Tasting Notes: {whisky.TastingNotes}
+        <br />
+        Region: {whisky.Region}
+        <br />
+        Description: {whisky.Description}
+      </Typography>
+    </CardContent>
+    <CardActions
+      disableSpacing
+      style={{ position: "relative", height: "100%" }}
+    >
+      <Button
+        variant="contained"
+        style={{
+          backgroundColor: "#E6E6FA",
+          color: "black",
+          left: "20px",
+        }}
+      >
+        Buy Now: ${whisky.BuyNowPrice}
+      </Button>
+      <div style={{ position: "absolute", right: "30px" }}>
+        <IconButton aria-label="view image">
+          <img src={ImageButtonAsset} alt="View" />
+        </IconButton>
+      </div>
+    </CardActions>
+  </Card>
+);
+
+export default WhiskyCard;
